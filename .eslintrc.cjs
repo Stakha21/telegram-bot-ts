02fc3a426daaf05cddeb5fa5d4e45135794052c9
unknown <pavelstaheev1@gmail.com>
2021-12-24 11:18:22 +0200

    deleted eslint ignore file, changed eslint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,7 @@ module.exports = {
     es2021: true,
     node: true,
   },
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-  ],
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 13,
@@ -16,9 +12,6 @@ module.exports = {
   plugins: ["@typescript-eslint"],
   ignorePatterns: "app.ts",
   rules: {
-    camelcase: [
-      "error",
-      { allow: ["chat_id", "message_id"], properties: "never" },
-    ],
+    "@typescript-eslint/no-non-null-assertion": "off",
   },
 };
